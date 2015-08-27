1a3eaa003add6901e99174ccadbd407dfaba84a8
Xiaofeng <hhhanxf@gmail.com>
2015-08-27 07:12:51 -0700

    adding items

--- app/src/main/java/org/xiaofeng/playground/MainActivity.java
+++ app/src/main/java/org/xiaofeng/playground/MainActivity.java
@@ -1,9 +1,11 @@
 package org.xiaofeng.playground;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.collapsing_toolbar_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, CollpasingToolBarActivity.class));
+			}
+		});
 	}
 
 	@Override
