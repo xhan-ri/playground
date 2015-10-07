60e6bfdfe46b77a53d615133073a9d715289c874
Xiaofeng <hhhanxf@gmail.com>
2015-10-07 07:29:07 -0700

    drawer activity.

--- app/src/main/java/org/xiaofeng/playground/MainActivity.java
+++ app/src/main/java/org/xiaofeng/playground/MainActivity.java
@@ -19,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
 				startActivity(new Intent(MainActivity.this, CollpasingToolBarActivity.class));
 			}
 		});
+		findViewById(R.id.drawer_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(MainActivity.this, DrawerActivity.class));
+			}
+		});
+//		startActivity(new Intent(MainActivity.this, DrawerActivity.class));
 		startActivity(new Intent(MainActivity.this, CollpasingToolBarActivity.class));
 	}
 
